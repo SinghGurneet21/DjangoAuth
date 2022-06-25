900f94773d456f66d9353ab4c6d0db6ce66183fb
Gurneet Singh <singhgurneet302@gmail.com>
2022-06-25 13:13:31 +0530

    Allowed Hosting

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django import http
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
-from requests import post
+
 from rsa import sign
 
 
